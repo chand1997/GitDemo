5e31ac2423b374c53e024b61c944416eb8cb992e
chand1997 <chanduchallahalli@gmail.com>
2022-12-18 11:16:59 +0530

    class A

--- src/org/code/A.java
+++ src/org/code/A.java
@@ -1,7 +1,7 @@
 package org.code;
 
-public class Main {
+public class A {
     public static void main(String[] args) {
-        System.out.println("yo ");
+        System.out.println("sjdyuydhdd");
     }
 }
